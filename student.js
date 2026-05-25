3dccd41e52229a26f27355828e4b1f7ae02e0fbd
Rehan Khan <rehankhan913724-commits>
2026-05-25 21:53:06 +0530

    frontend added

--- student.js
+++ student.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mysql = require("mysql2");
+const cors = require("cors")
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
@@ -25,20 +27,21 @@ db.connect((err) => {
     }
 });
 
+
 app.post("/addstudent", (req,res) => {
     const { name, rollno, clas } = req.body;
     if(!name || !rollno || !clas){
         return res.send("all field require!");
     }
     const sql = "INSERT INTO students(name, rollno, clas) VALUES(?, ?, ?)";
-    db.query(sql, [name, rollno, clas]), (err, results) => {
+    db.query(sql, [name, rollno, clas], (err, results) => {
         if(err){
             return res.send("Database error!");
         }
         else{
             return res.send("Student added successfully!");
         }
-    }
+    })
 });
 
 app.get("/liststudent", (req, res) => {
@@ -48,6 +51,33 @@ app.get("/liststudent", (req, res) => {
             return res.send("Database error!");
         }
         return res.json(results);
+    })
+});
+
+app.put("/updatestudent/:rollno", (req, res) => {
+    const rollno = req.params.rollno;
+
+    const {name, clas} = req.body;
+
+    const sql = " UPDATE students SET name=?, clas=?, WHERE rollno=?";
+    db.query(sql, [name, rollno, clas], (err, results) => {
+        if (err){
+            console.log(err)
+            res.send("Database Error!");
+        }
+        res.send(`rollno ${rollno} updated sucessfully`)
+    })
+});
+
+app.delete("/deletestudent/:rollno", (req, res) => {
+    const rollno = req.params.rollno;
+    const sql = "DELETE FROM students WHERE rollno=?";
+    db.query(sql, [rollno], (err, result) => {
+        if (err){
+            console.log(err);
+            res.send("Database Error!");
+        }
+        res.send(`rollno ${rollno} deleted sucessfully`)
     })
 });
 const port = process.env.port;
